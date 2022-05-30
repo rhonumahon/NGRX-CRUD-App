884ed7edce33a3590558a5b9fcb2ce314e4a256b
Rhon Umahon <65482394+rhonumahon@users.noreply.github.com>
2022-05-30 18:14:30 +0100

    Add router state

--- src/app/state/app-state.ts
+++ src/app/state/app-state.ts
@@ -1,7 +1,23 @@
-export interface AppState {}
+import { routerReducer, RouterReducerState } from "@ngrx/router-store";
+import { ActionReducerMap, createFeatureSelector, createSelector, MetaReducer } from "@ngrx/store";
+import { environment } from "src/environments/environment";
+
+export interface AppState {
+ 
+}
 
 //FOR LARGER APPLICATION example below
 // export interface AppState {
 //     app: AppState;
 //     customers: CustomerState;
 // }
+
+export const reducers: ActionReducerMap<AppState> = {
+    router: routerReducer
+}
+
+export const metaReducers: MetaReducer<AppState>[] = 
+    !environment.production ? [] : [];
+
+
+
